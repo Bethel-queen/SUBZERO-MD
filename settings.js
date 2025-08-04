b7b9ff9bf55ab2c02f75c2c3b979679c13b7319e
Bethel-queen <demyaamaxinrag@gmail.com>
2025-08-04 22:10:46 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;jNci3Tgb#kjAHK_AiWGw3f2D9K4RtvvhLUT2Kj5YMrR9D2lnK1Us", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;2NcVxLIa#uml5kaeZY3x2vtdOUJDgZx-OOlkcVWTc696D9Ll2vmU", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
