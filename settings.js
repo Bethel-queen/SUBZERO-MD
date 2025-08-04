44a782f8673edbb357408479123074d3ad3c4598
Bethel-queen <demyaamaxinrag@gmail.com>
2025-08-04 21:06:45 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;jNci3Tgb#kjAHK_AiWGw3f2D9K4RtvvhLUT2Kj5YMrR9D2lnK1Us", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
