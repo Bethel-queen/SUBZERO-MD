bc38c34c32960b483af97e0506610ec9ad4ed634
Bethel-queen <demyaamaxinrag@gmail.com>
2025-08-19 15:56:47 +0300

    settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "SUBZERO-MD;;;eB1BHIbA#Ieq1wA9g5o5JMboekeBDbdQl3KZEVBijqe5xZlbYSEs", // Put Session Id Here Inside "*"
+  SESSION_ID: "SUBZERO-MD;;;WB00wZ7I#qz5kE1QC5FAGsT7OlnWFZvvbwmS6dwJUtpJH4fIJIXU", // Put Session Id Here Inside "*"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
