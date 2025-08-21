bdce541abae49684cc0f2dffc0279f72433b7163
Bethel-queen <demyaamaxinrag@gmail.com>
2025-08-21 15:18:15 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // Put Session Id Here Inside "SUBZERO-MD;;;2NcVxLIa#uml5kaeZY3x2vtdOUJDgZx-OOlkcVWTc696D9Ll2vmU"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
